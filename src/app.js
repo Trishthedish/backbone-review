f0483ca693ac27d42a02aead2d30a420a2be2d36
Charles Ellis <hamled@hamled.name>
2016-11-30 11:51:41 -0800

    Views Part 1 - PageView

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 import $ from 'jquery';
+import Backbone from 'backbone';
+
+
+var PageView = Backbone.View.extend({
+  render: function() {
+    var cage = $('<img>');
+    cage.attr('src', 'https://www.placecage.com/200/300');
+    this.$el.append(cage);
+  }
+});
 
 $(document).ready(function() {
+  var page = new PageView({
+    el: $('body')
+  });
+  page.render();
   $('body').append('<h1>Hello Ada</h1>');
 });
